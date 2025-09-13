358b76bd0fa4e42303426ad50c182695081f9e2e
cjin3 <christopherqjin@gmail.com>
2025-09-12 21:33:05 -0400

    firebase

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,6 +1,29 @@
 import React from "react";
 import "./Home.css"; // import the stylesheet
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFunctions, httpsCallable } from "firebase/functions";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBiDixs8VRwm5PJyvJic8puhJTMwzb5ESA",
+  authDomain: "drawcmu-3daa1.firebaseapp.com",
+  projectId: "drawcmu-3daa1",
+  storageBucket: "drawcmu-3daa1.firebasestorage.app",
+  messagingSenderId: "181826924059",
+  appId: "1:181826924059:web:96d14b5f07f97abb1b52be",
+  measurementId: "G-RXM5255S3J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
 class Home extends React.Component {
   constructor(props) {
     super(props);
